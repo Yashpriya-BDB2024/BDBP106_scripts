0fd300e9aab02418e317622afe520e6a43045f71
Yash Priya Baid <yashpriyabaid@gmail.com>
2025-02-16 09:28:48 +0530

    Create lab15_ex1_Yashpriya.py

---
+++ Lab-15/lab15_ex1_Yashpriya.py
@@ -0,0 +1,10 @@
+#Get the alphabet from the user as input
+letter=input('Enter the alphabet: ')
+if letter=='a' or letter=='i'\
+        or letter=='o' or letter=='e'\
+        or letter=='u':
+        print('Entered letter is a vowel')
+elif letter=='y':
+    print('Sometimes its a vowel, and sometimes it is a consonant')
+else:
+    print('Consonant')
