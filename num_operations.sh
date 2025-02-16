636a228a90af546bc8b1bdc6dd61a00bcb7d7b5b
Yash Priya Baid <yashpriyabaid@gmail.com>
2025-02-16 10:05:03 +0530

    Delete num_operations.sh

--- num_operations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-Sum=$(( $1 +$2 ))
-echo "Sum of $1 and $2 is $Sum"
-Product=$(( $1 *$2 ))
-echo "Product of $1 and $2 is $Product" 
-Quotient=$(( $1 /$2 )) 
-echo "Quotient of $1 and $2 is $Quotient"
-Remainder=$(( $1 %$2 ))
-echo "Remainder of $1 and $2 is $Remainder"
