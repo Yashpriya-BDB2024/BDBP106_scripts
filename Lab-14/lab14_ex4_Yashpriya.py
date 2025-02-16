c3253866b2ad51a1a28de8498a7d78389663f86e
Yash Priya Baid <yashpriyabaid@gmail.com>
2025-02-16 09:30:57 +0530

    Create lab14_ex4_Yashpriya.py

---
+++ Lab-14/lab14_ex4_Yashpriya.py
@@ -0,0 +1,4 @@
+#Get your name as input
+name=input("Enter your name: ")
+#Print the statement - Hello and Welcome to the world of Python, name.
+print("Hello and Welcome to the world of Python,", name)
